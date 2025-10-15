c06713c7c5e33777a3a545fa0b5de6d2ebbe173d
taras <bronya2004@gmail.com>
2025-10-14 22:54:22 -0400

    Swagger bug fix

--- src/main/java/com/tarasantoniuk/finance/common/SwaggerGroupsConfig.java
+++ src/main/java/com/tarasantoniuk/finance/common/SwaggerGroupsConfig.java
@@ -7,30 +7,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerGroupsConfig {
 
-    @Bean
-    public GroupedOpenApi referenceDataGroup() {
-        return GroupedOpenApi.builder()
-                .group("reference-data")
-                .displayName("Reference Data")
-                .pathsToMatch(
-                        "/api/countries/**",
-                        "/api/currencies/**",
-                        "/api/exchange-rates/**"
-                )
-                .build();
-    }
-
-    @Bean
-    public GroupedOpenApi organizationGroup() {
-        return GroupedOpenApi.builder()
-                .group("organization")
-                .displayName("Organization Management")
-                .pathsToMatch(
-                        "/api/organizations/**",
-                        "/api/accounting-policies/**"
-                )
-                .build();
-    }
+//    @Bean
+//    public GroupedOpenApi referenceDataGroup() {
+//        return GroupedOpenApi.builder()
+//                .group("reference-data")
+//                .displayName("Reference Data")
+//                .pathsToMatch(
+//                        "/api/countries/**",
+//                        "/api/currencies/**",
+//                        "/api/exchange-rates/**"
+//                )
+//                .build();
+//    }
+//
+//    @Bean
+//    public GroupedOpenApi organizationGroup() {
+//        return GroupedOpenApi.builder()
+//                .group("organization")
+//                .displayName("Organization Management")
+//                .pathsToMatch(
+//                        "/api/organizations/**",
+//                        "/api/accounting-policies/**"
+//                )
+//                .build();
+//    }
 
 
 }
