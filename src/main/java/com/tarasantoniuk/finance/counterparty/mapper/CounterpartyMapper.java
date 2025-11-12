e81f145a76699faafb39d308aaaa1be6fe5e14f0
taras <bronya2004@gmail.com>
2025-11-12 13:55:19 +0100

    1. Add mapper tests for Counterparty 2. Fixed problem with idToCountry

--- src/main/java/com/tarasantoniuk/finance/counterparty/mapper/CounterpartyMapper.java
+++ src/main/java/com/tarasantoniuk/finance/counterparty/mapper/CounterpartyMapper.java
@@ -3,24 +3,40 @@ package com.tarasantoniuk.finance.counterparty.mapper;
 import com.tarasantoniuk.finance.counterparty.dto.CounterpartyRequestDto;
 import com.tarasantoniuk.finance.counterparty.dto.CounterpartyResponseDto;
 import com.tarasantoniuk.finance.counterparty.entity.Counterparty;
+import com.tarasantoniuk.finance.country.entity.Country;
 import com.tarasantoniuk.finance.country.mapper.CountryMapper;
 import org.mapstruct.*;
 
-@Mapper(componentModel = "spring", uses = {CountryMapper.class})
+import java.util.List;
+
+@Mapper(componentModel = "spring",
+        uses = {CountryMapper.class},
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface CounterpartyMapper {
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
-    @Mapping(target = "isActive", defaultValue = "true")
+    @Mapping(target = "country", source = "countryId", qualifiedByName = "idToCountry")
     Counterparty toEntity(CounterpartyRequestDto request);
 
-    @Mapping(source = "country", target = "country")
     CounterpartyResponseDto toResponse(Counterparty entity);
 
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    List<CounterpartyResponseDto> toResponseList(List<Counterparty> entities);
+
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "country", source = "countryId", qualifiedByName = "idToCountry")
     void updateEntity(CounterpartyRequestDto request, @MappingTarget Counterparty entity);
+
+    @Named("idToCountry")
+    default Country idToCountry(Long countryId) {
+        if (countryId == null) {
+            return null;
+        }
+        Country country = new Country();
+        country.setId(countryId);
+        return country;
+    }
 }
